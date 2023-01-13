4093176fb7c835d12480fb13126c97336d1c336c
jamesidala <idalajames@nvit.tech>
2023-01-13 15:21:32 +0100

    updated index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+def greet(name):
+    """This function greets the person passed in as a parameter"""
+    print("Hello, " + name + ". How are you today?")
+
+greet("John") # Output: Hello, John. How are you today?
+:
